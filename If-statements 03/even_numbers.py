68633b3b67227526483195de40ee0a66dd776a27
Khawaja Abdul Moiz <156238498+KhawajaAbdulMoiz@users.noreply.github.com>
2025-04-27 16:44:39 +0500

    Conditional Statements Practise Completed 🎉

---
+++ If-statements 03/even_numbers.py
@@ -0,0 +1,8 @@
+# Write a program that prints the first 20 even numbers. There are several correct approaches, but they all use a loop of some sort. Do no write twenty print statements
+
+
+def even():
+    for i in range(20):
+        print(i*2)
+
+even()
---
+++ If-statements 03/random_numbers.py
@@ -0,0 +1,15 @@
+
+# Print 10 random numbers in the range 1 to 100.
+
+# Here is an example run:
+
+# 45 79 61 47 52 10 16 83 19 12
+
+# Each time you run your program you should get different numbers
+
+# 81 76 70 1 27 63 96 100 32 92
+
+import random
+
+for i in range(100):
+    print(random.randint(0, i))
